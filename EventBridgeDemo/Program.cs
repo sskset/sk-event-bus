348accdf7e5964d2bba2e68acf4abe56af79fbbe
Shan <sskset@gmail.com>
2024-05-13 08:39:35 +1000

    WIP: add eventbridge

---
+++ EventBridgeDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Events;
+using SKEventBus.EventBridge;
+
+var eventBus = new EventBrdigeEventBus(
+    Environment.GetEnvironmentVariable("_endpointId")
+    );
+
+
+await eventBus.PublishAsync(new TestEvent1
+{
+    TestId = 1,
+    TestName = "Publish to aws eventbridge"
+});
+
+Console.WriteLine("Done");
+Console.ReadKey();
---
+++ SKEventBus.EventBridge/EventBrdigeEventBus.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.EventBridge;
+using Amazon.Runtime;
+using Newtonsoft.Json;
+
+namespace SKEventBus.EventBridge
+{
+    public class EventBrdigeEventBus : EventBus
+    {
+        private readonly string _endpointId;
+
+        public EventBrdigeEventBus(string endpointId)
+        {
+            _endpointId = endpointId;
+        }
+
+        public override async Task PublishAsync<TEvent>(TEvent @event)
+        {
+            var client = new AmazonEventBridgeClient();
+            var response = await client.PutEventsAsync(
+                new Amazon.EventBridge.Model.PutEventsRequest
+                {
+                    Entries = new List<Amazon.EventBridge.Model.PutEventsRequestEntry>
+                    {
+                        new Amazon.EventBridge.Model.PutEventsRequestEntry
+                        {
+                            Detail = JsonConvert.SerializeObject(@event),
+                            DetailType = @event.GetType().Name
+                        }
+                    },
+                    EndpointId = _endpointId,
+                });
+        }
+
+        public override Task StartListeningAsync()
+        {
+            // the EventBridge doesn't support pub/sub pattern
+            // the consumers have to be setup in the `Targets`
+            // check aws documentation to figure out how to configure `Targets`
+            throw new NotImplementedException();
+        }
+    }
+}
